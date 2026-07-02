9ebd9ebaa3b2490c1b20da637451749180bc4d21
AmitSheynhaft <214438947@idf.il>
2026-07-02 22:25:09 +0300

    fix: update API URL for client build and enhance CORS settings for HTTPS

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:5174',
       'http://10.10.248.140',
+      'https://10.10.248.140',
       'http://medsync',
+      'https://medsync',
     ],
   });
   await app.listen(process.env.PORT ?? 3000);
